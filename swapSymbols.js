9116ff55ac8932ff71ae8c1a599acde1429f4baa
Rogers Sampaio <r@allur.co>
2023-04-04 14:32:45 -0300

    FInal simple script

--- swapSymbols.js
+++ swapSymbols.js
@@ -1,21 +1,23 @@
+
 // Get active document
 var doc = app.activeDocument;
 
 // Get all layers in the document
 var layers = doc.layers;
 log("Start!");
-log("Layer: " + JSON.stringify(layers));
+log("Layer Size: " + layers.length);
 
 // Iterate over each layer in the document
 for (var i = 0; i < layers.length; i++) {
   // Get the sublayers in the current layer
-  var sublayers = layers[i].layers;
+  var sublayers = layers[i].symbolItems;
+  log("Layer: " + layers[i].name + " Sublayer: " + sublayers.length);
 
   // Iterate over each sublayer
   for (var j = 0; j < sublayers.length; j++) {
     // Get the symbol in the current sublayer
-    log("Layer: " + JSON.stringify(sublayers[j]));
-    var symbol = sublayers[j].symbolItems[0].symbol;
+    log("Layer: " + layers[i].name + " Sublayer: " + sublayers[j].name);
+    var symbol = sublayers[j];
 
 
     // Get a random symbol from the document
@@ -24,7 +26,7 @@ for (var i = 0; i < layers.length; i++) {
     var randomSymbol = symbols[randomSymbolIndex];
 
     // Swap the current symbol with the random one
-    sublayers[j].symbolItems[0].symbol = randomSymbol;
+    sublayers[j].symbol = randomSymbol;
 
     // Rename the sublayer to match the new symbol name
     sublayers[j].name = randomSymbol.name;
