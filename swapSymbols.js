71e497c23a497c91d254b49335c2b2fda222baf8
Rogers Sampaio <r@allur.co>
2023-04-04 12:48:41 -0300

    Sublayers

--- swapSymbols.js
+++ swapSymbols.js
@@ -1,35 +1,31 @@
 // Get active document
 var doc = app.activeDocument;
 
-// Get the first row of layers
-var firstRow = doc.layers.slice(0, 20);
+// Get all layers in the document
+var layers = doc.layers;
 
-// Duplicate the first row and move it down
-var secondRow = firstRow.duplicate(doc, ElementPlacement.PLACEAFTER);
-var secondRowTop = secondRow[0].top;
-for (var i = 0; i < secondRow.length; i++) {
-    secondRow[i].translate(0, 100);
-}
+// Iterate over each layer in the document
+for (var i = 0; i < layers.length; i++) {
+  // Get the sublayers in the current layer
+  var sublayers = layers[i].layers;
 
-// Get all symbols in the document
-var symbols = doc.symbols;
+  // Iterate over each sublayer
+  for (var j = 0; j < sublayers.length; j++) {
+    // Get the symbol in the current sublayer
+    var symbol = sublayers[j].symbolItems[0].symbol;
 
-// Iterate over each layer in the second row
-for (var i = 0; i < secondRow.length; i++) {
-  // Get the symbol in the current layer
-  var symbol = secondRow[i].symbolItems[0].symbol;
+    // Get a random symbol from the document
+    var symbols = doc.symbols;
+    var randomSymbolIndex = Math.floor(Math.random() * symbols.length);
+    var randomSymbol = symbols[randomSymbolIndex];
 
-  // Get a random symbol from the document
-  var randomSymbolIndex = Math.floor(Math.random() * symbols.length);
-  var randomSymbol = symbols[randomSymbolIndex];
+    // Swap the current symbol with the random one
+    sublayers[j].symbolItems[0].symbol = randomSymbol;
 
-  // Swap the current symbol with the random one
-  secondRow[i].symbolItems[0].symbol = randomSymbol;
-
-  // Rename the layer to match the new symbol name
-  secondRow[i].name = randomSymbol.name;
+    // Rename the sublayer to match the new symbol name
+    sublayers[j].name = randomSymbol.name;
+  }
 }
 
 // Refresh the document
 doc.select(null);
-doc.selection = [secondRow];
